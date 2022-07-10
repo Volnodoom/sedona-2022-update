03650140b04fb685457f15cf96316448272cf0a0
Alexander <bdiyoz@gmail.com>
2022-07-10 15:41:26 +0400

    fix: place condition on closing modal window

--- js/modal-window.js
+++ js/modal-window.js
@@ -3,12 +3,25 @@ const searchBtn = mainPage.querySelector('.hotel-search__button-initialization')
 const modalWindow = document.querySelector('.modal');
 const modalOutside = modalWindow.querySelector('.modal__overlay');
 const submitBtn = modalWindow.querySelector('.modal-search-form')
+const title = modalWindow.querySelector('h2');
+const form = modalWindow.querySelector('form');
 
 const handleModalClick = () => {
   modalWindow.classList.toggle('is-active');
 }
 
-const handleModalOutsideClick = () => {
+const handleModalOutsideClick = (evt) => {
+  const target = evt.target;
+  const currentTarget = evt.currentTarget;
+
+  if(target === title) {
+    return;
+  }
+
+  if(currentTarget === form) {
+    return;
+  }
+
   modalWindow.classList.toggle('is-active');
 }
 
@@ -18,5 +31,5 @@ const handleFormSubmit = (evt) => {
 }
 
 searchBtn.addEventListener('click', handleModalClick);
-modalOutside.addEventListener('click', handleModalClick);
+modalWindow.addEventListener('click', handleModalOutsideClick);
 submitBtn.addEventListener('click', handleFormSubmit);
